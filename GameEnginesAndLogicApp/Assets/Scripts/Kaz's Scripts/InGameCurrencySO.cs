426dcb4f5c48d1dd233913599866c32c92aa5246
EyepatchDoge <kaspo.eyepatch@gmail.com>
2020-03-20 21:04:16 -0400

    kas_update_08
    Created scriptable objects for currency and beginning to write script for non app store purchases

---
+++ GameEnginesAndLogicApp/Assets/Scripts/Kaz's Scripts/InGameCurrencySO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameCurrencySO", menuName = "ScriptableObjects/GameCurency", order = 1)]
+public class InGameCurrencySO : ScriptableObject
+{
+    public string currencyName;
+    public Sprite currency;
+    public int currencyAmount;
+}
