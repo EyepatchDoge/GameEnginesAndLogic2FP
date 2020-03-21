102e1bd8e78843a775b86ae78cf459fac6abc6bb
Pauldrummondo <pauldrummond.biz@gmail.com>
2020-03-20 20:34:29 -0400

    tink
    tried tinkering with the getTouch, still cant figure out how to get the player to fly on touch commant

--- GameEnginesAndLogicApp/Assets/Scripts/Paul_Scripts/PaulPlayer.cs
+++ GameEnginesAndLogicApp/Assets/Scripts/Paul_Scripts/PaulPlayer.cs
@@ -85,4 +85,8 @@ public class PaulPlayer : MonoBehaviour
         playerIsDed = false;
         GameManager.instance.playDed = false;
     }
+    //public void MightAsWellJump()
+    //{
+
+    //}
 }
